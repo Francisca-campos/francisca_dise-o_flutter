b0d2def2e17314f45fe34e13ddfd513d7238c712
Francisca <al221811678@gmail.com>
2020-12-08 19:28:43 -0600

    diseño 3

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -6,41 +6,55 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        _crearImagen(),
-        _crearTitulo(),
-        _crearAcciones(),
-        _crearTexto(),
-      ],
+        body: SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _crearImagen(),
+          _crearTitulo(),
+          _crearAcciones(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+          _crearTexto(),
+        ],
+      ),
     ));
   }
 
   Widget _crearImagen() {
-    return Image(
-      image: NetworkImage(
-          'https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/747964/pexels-photo-747964.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+        height: 180.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Lago con un puente', style: estiloTitulo),
-                SizedBox(height: 7.0),
-                Text('Un lago en Alemania', style: estiloSubtitulo),
-              ],
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Lago con un puente', style: estiloTitulo),
+                  SizedBox(height: 7.0),
+                  Text('Un lago en Alemania', style: estiloSubtitulo),
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, color: Colors.red, size: 30.0),
-          Text('41', style: TextStyle(fontSize: 20.0))
-        ],
+            Icon(Icons.star, color: Colors.red, size: 30.0),
+            Text('41', style: TextStyle(fontSize: 20.0))
+          ],
+        ),
       ),
     );
   }
@@ -67,11 +81,13 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTexto() {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Occaecat est incididunt ullamco Lorem veniam velit non ipsum ea. Esse laborum minim labore nisi aliquip aliquip aliquip elit Lorem dolore. Exercitation sit labore dolor voluptate nostrud cillum culpa ut cillum culpa nisi in. Duis do aliqua commodo velit consectetur aute nostrud deserunt. Fugiat culpa sunt proident enim ullamco quis laboris laboris dolore. Veniam nisi adipisicing dolore proident ullamco veniam aute aute enim culpa cillum amet deserunt. Reprehenderit cillum excepteur do anim consequat ex sunt deserunt nulla.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0),
+        child: Text(
+          'Occaecat est incididunt ullamco Lorem veniam velit non ipsum ea. Esse laborum minim labore nisi aliquip aliquip aliquip elit Lorem dolore. Exercitation sit labore dolor voluptate nostrud cillum culpa ut cillum culpa nisi in. Duis do aliqua commodo velit consectetur aute nostrud deserunt. Fugiat culpa sunt proident enim ullamco quis laboris laboris dolore. Veniam nisi adipisicing dolore proident ullamco veniam aute aute enim culpa cillum amet deserunt. Reprehenderit cillum excepteur do anim consequat ex sunt deserunt nulla.',
+          textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
