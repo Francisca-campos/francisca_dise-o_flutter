8357e05fc6dc96f888f363beb83b342e7093d42c
Francisca <al221811678@gmail.com>
2020-12-08 23:30:46 -0600

    diseño compuesto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:francisca_diseno_flutter/src/pages/basico_page.dart';
 import 'package:francisca_diseno_flutter/src/pages/botones_page.dart';
 import 'package:francisca_diseno_flutter/src/pages/scroll_page.dart';
@@ -8,6 +9,9 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle.light.copyWith(statusBarColor: Colors.white));
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
